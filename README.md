988be6563412d8e3ee9169212197999a75ee5416
Luis Maya Aranda <mayafluis@gmail.com>
2021-09-18 15:33:50 -0700

    Init repo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fast-sum-of-proper-divisors
-Very very very fast way to find the sum of proper divisors
+Very very very fast way to find the sum of proper divisors using c.
+
+Must include [primes.h](./primes.h).
---
+++ fast.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include "primes.h"
+
+unsigned int fast_sum(unsigned int num) {
+    unsigned int sum, x, sub;
+
+    sum = 1;
+    x = num;
+    for (int i = 0; i < P_COUNT; ++i) {
+        if (P[i] > x / P[i])
+            break;
+
+        if (x % P[i] == 0) {
+            sub = P[i] + 1;
+            x /= P[i];
+            while (x % P[i] == 0) {
+                x /= P[i];
+                sub = sub * P[i] + 1;
+            }
+            sum *= sub;
+        }
+    }
+    if (x > 1)
+        sum *= x + 1;
+
+    sum = sum - num;
+    return sum;
+}
+
+int main(int argc, char *argv[]) {
+    unsigned int n = 21387312;
+    printf("Sum of proper divisors for (%d): %d\n", n, fast_sum(n));
+    return 0;
+}
