b8636ce0d6934f2b5ef9f64e25e30d8a84cd1c12
Luis Maya Aranda <mayafluis@gmail.com>
2021-09-18 15:42:30 -0700

    Cleaning stuff up

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # fast-sum-of-proper-divisors
-Very very very fast way to find the sum of proper divisors using c.
+Very very very fast way to find the sum of proper divisors using `C`.
 
-Must include [primes.h](./primes.h).
+Function for computing the sum of proper divisors is `fast_sum` at the top of [fast.c](./fast.c)
+Must also include [primes.h](./primes.h) due to use of decomposition in prime factors in the algorithm.
--- fast.c
+++ fast.c
@@ -2,15 +2,14 @@
 
 #include "primes.h"
 
+/* Uses decomposition in prime factors for fast sum of proper divisors */
 unsigned int fast_sum(unsigned int num) {
     unsigned int sum, x, sub;
 
     sum = 1;
     x = num;
     for (int i = 0; i < P_COUNT; ++i) {
-        if (P[i] > x / P[i])
-            break;
-
+        if (P[i] > x / P[i]) break;
         if (x % P[i] == 0) {
             sub = P[i] + 1;
             x /= P[i];
@@ -21,11 +20,8 @@ unsigned int fast_sum(unsigned int num) {
             sum *= sub;
         }
     }
-    if (x > 1)
-        sum *= x + 1;
-
-    sum = sum - num;
-    return sum;
+    if (x > 1) sum *= x + 1;
+    return sum - num;
 }
 
 int main(int argc, char *argv[]) {
